acefd72fc2d5b02cd1eeae627c010686bb602b18
kishore-3101 <purukiss@gmail.com>
2026-08-18 23:32:28 +0530

    fix: configure server for deployment

--- backend/server.js
+++ backend/server.js
@@ -68,6 +68,6 @@ app.post("/api/contact", async (req, res) =>{
 
 const PORT=process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running in the port ${PORT}`);
 });
